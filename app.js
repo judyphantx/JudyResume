0446ac1ee960c0554854ec98f201f24e351c9b67
Judy Phan <judy.phan2@g.austincc.edu>
2025-01-23 22:18:29 -0600

    Added functionality to button in javascript file

---
+++ app.js
@@ -0,0 +1,7 @@
+const menuToggle =document.querySelector('.navbar_toggle');
+const menuLinks = document.querySelector('.navbar_menu');
+
+menuToggle.addEventListener('click', function(){
+    menuToggle.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
